86ac0c9a122eb50359a86d5bda5988966a15f500
raf <rraf@tuta.io>
2023-04-29 13:14:28 +0100

    update: got xml parsing working using serde

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123
 use curl::easy::{ Easy2, Handler, WriteError };
+use serde::Deserialize;
+use serde_xml_rs::from_str;
 
 fn main() {
   let mut curl = Easy2::new(Collector(Vec::new()));
@@ -8,8 +10,10 @@ fn main() {
   curl.perform().unwrap();
 
   assert_eq!(curl.response_code().unwrap(), 200);
-  let xml = &curl.get_ref().0;
-  println!("{}", String::from_utf8_lossy(xml));
+  let xml = String::from_utf8_lossy(&curl.get_ref().0);
+
+  let test: Rss = from_str(&xml).unwrap();
+  println!("{}", test.channel.title);
 }
 
 struct Collector(Vec<u8>);
@@ -18,4 +22,18 @@ impl Handler for Collector {
     self.0.extend_from_slice(data);
     Ok(data.len())
   }
+}
+
+#[derive(Debug, Deserialize)]
+struct Rss {
+  channel: Channel
+}
+
+#[derive(Debug, Deserialize)]
+struct Channel {
+  title: String,
+  // link: String,
+  description: String,
+  // language: String,
+  copyright: String
 }
