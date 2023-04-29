fcee7268144e7ee616a59c1121c3315003a3f47d
raf <rraf@tuta.io>
2023-04-29 15:46:14 +0100

    update: we aren't stuck to leeds anymore

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,24 @@ use rss::Rss;
 use colored::Colorize;
 use curl::easy::{ Easy2, Handler, WriteError };
 use serde_xml_rs::from_str;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Arguments {
+  #[arg(short, long)]
+  location: String
+}
 
 fn main() {
+  let arguments = Arguments::parse();
+
   let mut curl = Easy2::new(Collector(Vec::new()));
   curl.get(true).unwrap();
-  curl.url("https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2644688").unwrap();
+
+  let mut link: String = String::from("https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/").to_owned();
+  link.push_str(&bbc_location::get_location_code(arguments.location).to_string());
+
+  curl.url(&link).unwrap() ;
   curl.perform().unwrap();
   assert_eq!(curl.response_code().unwrap(), 200);
 
@@ -20,8 +33,6 @@ fn main() {
   println!("{}", test.channel.description.white());
   println!("{}", test.channel.items[0].title);
   println!("{}", test.channel.items[0].description);
-
-  println!("{}", bbc_location::Location::Leeds as u32);
 }
 
 struct Collector(Vec<u8>);
@@ -33,7 +44,11 @@ impl Handler for Collector {
 }
 
 mod bbc_location {
-  pub enum Location {
-    Leeds = 2644688,
+  pub fn get_location_code(location: String) -> u32 {
+    if location.to_lowercase() == "leeds" { return 2644688 }
+    if location.to_lowercase() == "bradford" { return 2654993 }
+    if location.to_lowercase() == "manchester" { return 2643123 }
+    if location.to_lowercase() == "sheffield" { return 2638077 }
+    else { return 0 }
   }
 }
