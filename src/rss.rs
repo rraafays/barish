99a3ad16b6864b1bec4fd5523792ac616408803f
raf <rraf@tuta.io>
2023-07-26 11:28:10 +0100

    chore: reformatting

--- src/rss.rs
+++ src/rss.rs
@@ -2,19 +2,19 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct Rss {
-    pub channel: Channel,
+  pub channel: Channel,
 }
 
 #[derive(Deserialize)]
 pub struct Channel {
-    pub title: String,
-    pub description: String,
-    #[serde(rename = "item")]
-    pub items: [Item; 3],
+  pub title: String,
+  pub description: String,
+  #[serde(rename = "item")]
+  pub items: [Item; 3],
 }
 
 #[derive(Deserialize)]
 pub struct Item {
-    pub title: String,
-    pub description: String,
+  pub title: String,
+  pub description: String,
 }
