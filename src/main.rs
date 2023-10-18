68a7069b47554c318991a0677479207ccf19f2be
raf <rraf@tuta.io>
2023-10-18 14:21:34 +0100

    chore: reformatting

--- src/main.rs
+++ src/main.rs
@@ -13,46 +13,45 @@ use serde_xml_rs::from_str;
 
 #[derive(Parser)]
 struct Arguments {
-  #[arg(short, long, default_value_t = false)]
-  verbose: bool,
-  #[arg(short, long)]
-  location: String,
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+    #[arg(short, long)]
+    location: String,
 }
 
 fn main() {
-  let arguments = Arguments::parse();
+    let arguments = Arguments::parse();
 
-  let mut curl = Easy2::new(Collector(Vec::new()));
-  curl.get(true).unwrap();
+    let mut curl = Easy2::new(Collector(Vec::new()));
+    curl.get(true).unwrap();
 
-  let mut link: String =
-    String::from("https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/").to_owned();
-  link.push_str(&get_location_code(arguments.location).to_string());
+    let mut link: String =
+        String::from("https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/").to_owned();
+    link.push_str(&get_location_code(arguments.location).to_string());
 
-  curl.url(&link).unwrap();
-  curl.perform().unwrap();
+    curl.url(&link).unwrap();
+    curl.perform().unwrap();
 
-  if curl.response_code().unwrap() != 200 {
-    println!("{} please provide a valid location!", "error:".red());
-    return;
-  }
-
-  let xml = String::from_utf8_lossy(&curl.get_ref().0);
-  let rss: Rss = from_str(&xml).unwrap();
-
-  let mut label: &str;
-  let mut colour_value: u8 = 4; // blue
-  for item in rss.channel.items.iter() {
-    label = item.title.split_whitespace().next().unwrap_or("");
-    println!(
-      "{}",
-      item
-        .title
-        .replace(label, &label.color(get_colour(colour_value)).to_string())
-    );
-    if arguments.verbose {
-      println!("{}\n", item.description.replace(", ", "\n").bright_black());
+    if curl.response_code().unwrap() != 200 {
+        println!("{} please provide a valid location!", "error:".red());
+        return;
     }
-    colour_value += 1;
-  }
+
+    let xml = String::from_utf8_lossy(&curl.get_ref().0);
+    let rss: Rss = from_str(&xml).unwrap();
+
+    let mut label: &str;
+    let mut colour_value: u8 = 4; // blue
+    for item in rss.channel.items.iter() {
+        label = item.title.split_whitespace().next().unwrap_or("");
+        println!(
+            "{}",
+            item.title
+                .replace(label, &label.color(get_colour(colour_value)).to_string())
+        );
+        if arguments.verbose {
+            println!("{}\n", item.description.replace(", ", "\n").bright_black());
+        }
+        colour_value += 1;
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,22 +2,22 @@ use colored::Color;
 use curl::easy::{Handler, WriteError};
 
 pub fn get_colour(colour: u8) -> Color {
-  match colour {
-    0 => Color::BrightBlack,
-    1 => Color::BrightRed,
-    2 => Color::BrightGreen,
-    3 => Color::BrightYellow,
-    4 => Color::BrightBlue,
-    5 => Color::BrightMagenta,
-    6 => Color::BrightCyan,
-    _ => Color::BrightWhite,
-  }
+    match colour {
+        0 => Color::BrightBlack,
+        1 => Color::BrightRed,
+        2 => Color::BrightGreen,
+        3 => Color::BrightYellow,
+        4 => Color::BrightBlue,
+        5 => Color::BrightMagenta,
+        6 => Color::BrightCyan,
+        _ => Color::BrightWhite,
+    }
 }
 
 pub struct Collector(pub Vec<u8>);
 impl Handler for Collector {
-  fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
-    self.0.extend_from_slice(data);
-    Ok(data.len())
-  }
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0.extend_from_slice(data);
+        Ok(data.len())
+    }
 }
